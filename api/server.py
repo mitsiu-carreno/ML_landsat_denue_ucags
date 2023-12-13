c1a7651b5f5c0390b74174a66af80ae9a7c8846b
MarcoRodriguezR <marco.rodriguez@alumnos.ucags.edu.mx>
2023-12-13 01:11:18 -0600

    Agregar CORS al servidor

--- api/server.py
+++ api/server.py
@@ -7,26 +7,27 @@ import rasterio
 from shapely.geometry import Point
 from pyproj import Proj, Transformer
 import numpy as np
-
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-model = keras.models.load_model('assets/geo_recortes.keras')
+CORS(app)
+model = keras.models.load_model("assets/geo_recortes.keras")
 ruta_landsat_tiff = "assets/imagen.tif"
+
 
 @app.route("/")
 def hello_world():
     return "<p>Usage:</br>url/predict?lat=<XXXX:float>&lon=<YYYY:float></p>"
 
-@app.route('/predict', methods=['GET'])
+
+@app.route("/predict", methods=["GET"])
 def search():
     args = request.args
-    lat = args.get('lat')
-    lon = args.get('lon')
+    lat = args.get("lat")
+    lon = args.get("lon")
     print(lon)
 
-    if  None in (lat, lon):
+    if None in (lat, lon):
         abort(400)
 
     try:
@@ -54,25 +55,31 @@ def search():
 @app.errorhandler(400)
 def bad_request(e):
     # note that we set the 404 status explicitly
-    return "<h1>Missing parameters</h1><p>You <strong>must</strong> specify lat and lon</p></br><p>Usage:</br>url/predict?lat=<XXXX:float>&lon=<YYYY:float></p>", 400
+    return (
+        "<h1>Missing parameters</h1><p>You <strong>must</strong> specify lat and lon</p></br><p>Usage:</br>url/predict?lat=<XXXX:float>&lon=<YYYY:float></p>",
+        400,
+    )
+
 
 @app.errorhandler(406)
 def not_acceptable(e):
-    return "<h1>Wrong parameters</h1><p>Lat and lon parameters should be <strong>numeric</strong> values</p></br><p>Usage:</br>url/predict?lat=<XXXX:float>&lon=<YYYY:float></p>", 406
+    return (
+        "<h1>Wrong parameters</h1><p>Lat and lon parameters should be <strong>numeric</strong> values</p></br><p>Usage:</br>url/predict?lat=<XXXX:float>&lon=<YYYY:float></p>",
+        406,
+    )
 
 
 def GetPixelFromCoords(lat, lon):
     with rasterio.open(ruta_landsat_tiff) as src:
         srs_raster = src.crs.to_proj4()
 
-    #xy = zip(lon, lat)
+    # xy = zip(lon, lat)
     geometry = Point(lon, lat)
 
     srs_gdf = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
     transformador = Transformer.from_proj(Proj(srs_gdf), Proj(srs_raster))
 
     geometry2 = Point(transformador.transform(geometry.x, geometry.y))
-
 
     with rasterio.open(ruta_landsat_tiff) as src:
         col_pixel, row_pixel = src.index(geometry2.x, geometry2.y)
@@ -81,20 +88,23 @@ def GetPixelFromCoords(lat, lon):
 
 
 def GetImageCutout(col, row, size=3):
-    _5p = np.empty([5,5,6])
-    _3p = np.empty([3,3,6])
+    _5p = np.empty([5, 5, 6])
+    _3p = np.empty([3, 3, 6])
     with rasterio.open(ruta_landsat_tiff) as src:
         # 5 Pixels
         for sub_row in range(0, 5):
             for sub_col in range(0, 5):
                 _5p[sub_row][sub_col] = src.read(
-                    window=((row + (sub_row -2), row + (sub_row -1)),
-                            (col + (sub_col -2), col + (sub_col -1)))
+                    window=(
+                        (row + (sub_row - 2), row + (sub_row - 1)),
+                        (col + (sub_col - 2), col + (sub_col - 1)),
+                    )
                 ).reshape(_5p.shape[-1:])
-    _3p = _5p[1:4,1:4,:]
+    _3p = _5p[1:4, 1:4, :]
 
     return _3p
 
+
 def Predict(cutout):
-    cutout = cutout.reshape(1,3,3,6)
+    cutout = cutout.reshape(1, 3, 3, 6)
     return model.predict(cutout)
